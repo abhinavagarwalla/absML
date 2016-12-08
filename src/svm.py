c167303a4751a9ce6fcf93004eb15fa18732c36a
Dhruv Apte <dhruvgirishapte@gmail.com>
2016-12-09 00:05:24 +0530

    SVM char recognition

---
+++ src/svm.py
@@ -0,0 +1,16 @@
+#a simple support vector machine with char recognition
+import matplotlib.pyplot as plt
+from sklearn import datasets
+from sklearn import svm
+digits = datasets.load_digits()
+print(digits.data)
+
+print(digits.target)
+clf = svm.SVC(gamma= 0.004,C=100)
+X,y = digits.data[:-10],digits.target[:-10]
+clf.fit(X,y)
+
+print(clf.predict(digits.data[-5]))
+plt.imshow(digits.images[-5],cmap=plt.cm.gray_r,interpolation='nearest')
+
+plt.show()
